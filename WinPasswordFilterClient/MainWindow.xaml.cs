2844a6795b4346e7946c0702aa1ce67af64dc39f
vuletic <vuletic93@gmail.com>
2017-07-22 20:46:31 +0200

    Reading and writing settings implemented

--- WinPasswordFilterClient/MainWindow.xaml.cs
+++ WinPasswordFilterClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,20 @@ namespace WinPasswordFilterClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string _settingsFile = @"C:\WinPasswordFilter\settings.txt";
+        
         public MainWindow()
         {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
+            try
+            {
+                readSettings(_settingsFile);
+            }
+            catch (Exception)
+            {
+                writeSettings(_settingsFile);
+            }
         }
 
         private void editPartialDictionaryButton_Click(object sender, RoutedEventArgs e)
@@ -37,7 +49,15 @@ namespace WinPasswordFilterClient
 
         private void applyButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                writeSettings(_settingsFile);
+                MessageBox.Show("Successfully applied settings.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There was an error while applying settings.");
+            }
         }
 
         private void closeMenuItem_Click(object sender, RoutedEventArgs e)
@@ -47,12 +67,12 @@ namespace WinPasswordFilterClient
 
         private void loadMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            //TODO: open file dialog and then call readsettings
         }
 
         private void saveMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            //TODO: save file dialog and then call writesettings
         }
 
         private void partialMatchMenuItem_Click(object sender, RoutedEventArgs e)
@@ -67,7 +87,36 @@ namespace WinPasswordFilterClient
 
         private void aboutMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            // TODO: info about project, author, description
         }
+
+        private void readSettings(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            
+            partialMatchDictionaryValue.IsChecked = lines[0].Equals("1");
+            exactMatchDictionaryValue.IsChecked = lines[1].Equals("1");
+            userDataValue.IsChecked = lines[2].Equals("1");
+            minCharactersValue.Value = Int32.Parse(lines[3]);
+            maxCharactersValue.Value = Int32.Parse(lines[4]);
+        }
+
+        private void writeSettings(string path)
+        {
+            string lines = "";
+
+            lines += (bool)partialMatchDictionaryValue.IsChecked ? "1\r\n" : "0\r\n";
+            lines += (bool)exactMatchDictionaryValue.IsChecked ? "1\r\n" : "0\r\n";
+            lines += (bool)userDataValue.IsChecked ? "1\r\n" : "0\r\n";
+            lines += minCharactersValue.Value + "\r\n";
+            lines += maxCharactersValue.Value + "\r\n";
+
+            System.IO.FileInfo file = new System.IO.FileInfo(path);
+            file.Directory.Create();
+            System.IO.File.WriteAllText(file.FullName, lines);
+            
+        }
+
+
     }
 }
