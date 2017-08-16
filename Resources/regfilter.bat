c40b8217ba7d428a325cb2091b49d674226009cb
vuletic <vuletic93@gmail.com>
2017-08-16 14:04:30 +0200

    setup updated

--- Resources/regfilter.bat
+++ Resources/regfilter.bat
@@ -1,2 +1,6 @@
 reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v "Notification Packages" /t REG_MULTI_SZ /d scecli\0WinPasswordFilter /f
+mkdir C:\WinPasswordFilter
+icacls C:\WinPasswordFilter /inheritance:d
+icacls C:\WinPasswordFilter /remove:g Users
+icacls C:\WinPasswordFilter /remove:g "Authenticated Users"
 exit
